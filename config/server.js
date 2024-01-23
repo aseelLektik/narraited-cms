24154f626c9d57db48991f2b5eb3dd95be84dee9
aseelkp <aseelaroor10@gmail.com>
2024-01-23 17:28:37 +0530

    update : add server url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('MY_SERVER_URL', 'https://172.203.179.85:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '00a3ba90c87a9953a6a98fa6db5fbc30'),
